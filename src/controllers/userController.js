e5359ec5f60d502b409fc0664105acf8c6b2bdc2
Vitor Torii <vitortorii@gmail.com>
2022-12-23 01:59:44 -0300

    feat: get users/me

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,35 @@
-export async function getUsers(req, res) {}
+import connection from "../database/db.js";
+
+export async function getUsers(req, res) {
+  const userId = res.locals.userId;
+
+  try {
+    const object = await connection.query(
+      `
+      SELECT
+        users.id,
+        users.name,
+        SUM(urls."visitCount") AS "visitCount",
+        json_agg(
+          json_build_object(
+            'id', urls.id,
+            'shortUrl', urls."shortUrl",
+            'url', urls.url, 'visitCount',
+            urls."visitCount"
+          )
+        ) AS "shortenedUrls"
+      FROM users
+      JOIN urls
+      ON urls."userId" = users.id
+      WHERE users.id = $1
+      GROUP BY users.id
+      `,
+      [userId]
+    );
+
+    res.send(object.rows).status(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -1,1 +1,31 @@
-export async function getUsersVal(req, res, next) {}
+import connection from "../database/db.js";
+
+export async function getUsersVal(req, res, next) {
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  try {
+    const verifySession = await connection.query(
+      `
+      SELECT *
+      FROM sessions
+      WHERE token = $1
+      `,
+      [token]
+    );
+
+    if (verifySession.rows.length === 0) {
+      return res.sendStatus(404);
+    }
+
+    const userId = verifySession.rows[0].userId;
+    res.locals.userId = userId;
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+  next();
+}
